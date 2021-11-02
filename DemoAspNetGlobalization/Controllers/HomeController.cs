afb79bdaadc4f9641ffe93d0924491f456828800
Fabio de Stefani <fabiostefani@gmail.com>
2021-11-02 10:32:00 -0300

    Demo

---
+++ DemoAspNetGlobalization/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using DemoAspNetGlobalization.Resources;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+
+namespace DemoAspNetGlobalization.Controllers
+{
+    [ApiController]  
+    [Route("Home")]
+    public class HomeController : ControllerBase
+    {
+        //https://localhost:44336/home?culture=en-US
+        //
+        //https://localhost:44336/home
+        [HttpGet]
+        public IActionResult Get([FromServices] IStringLocalizer<Messages> localizer)
+        {
+            return Ok(new
+            {
+                Message = localizer["HelloWorld"].Value,
+                Message2 = localizer["Teste"].Value,
+                Date = DateTime.Now,
+                DateUtl = DateTime.UtcNow
+            });
+        }
+    }
+}
---
+++ DemoAspNetGlobalization/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace DemoAspNetGlobalization
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DemoAspNetGlobalization", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DemoAspNetGlobalization v1"));
+            }
+
+            app.UseHttpsRedirection();
+            var supportedCultures = new[] { "pt-BR", "en-US" };
+            var locatizationOptions = new RequestLocalizationOptions()
+                .SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            app.UseRequestLocalization(locatizationOptions);
+
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
